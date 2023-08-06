1e7ef921e7e97e1eee9600f830ca5d51c6ad2fc6
monocuco01 <119557633+monocuco01@users.noreply.github.com>
2023-08-06 14:58:34 -0500

    para filtros

--- PF-FrontEnd/src/components/store/Store.jsx
+++ PF-FrontEnd/src/components/store/Store.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Filters from "../filters/Filters";
-import "./Store.css"
+import "./Store.css";
+import Cards from "../Cards/Cards";
+import { useDispatch } from "react-redux";
+
+
 const Store = () => {
   return (
     <div className="store-wrapper home-wrapper-2 py-5">
-      <div className="container-xxl">
-        <div className="row"></div>
-        <Filters />
+      <div className="pruebitaFlex container-xxl">
+        <div className="row">
+          <Filters />
+          <Cards products={}/> 
+        </div>
       </div>
     </div>
   );
