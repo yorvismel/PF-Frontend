5dfafea3d2667307be40ae926d93fd26a601bfd4
Th0mcar <th0mcar@outlook.com>
2023-08-07 15:22:42 -0300

    login

--- PF-FrontEnd/src/App.jsx
+++ PF-FrontEnd/src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import HeaderMenu from "./components/HeaderMenu/HeaderMenu";
@@ -6,19 +8,38 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from "./components/Footer/Footer";
 import { Detail } from "./components/Detail/Detail";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
 import Store from "./components/store/Store";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 function App() {
+  const navigate = useNavigate(); // Obtiene la función de navegación
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado de inicio de sesión
+
+  const handleLogin = (user) => {
+    // Simulate successful login for demonstration purposes
+    // In a real application, you would perform actual authentication here
+    console.log("Logged in user:", user);
+    setIsLoggedIn(true); 
+    navigate("/home"); 
+  };
+
   return (
     <div className="toditotodito">
-      <Header />
-      <HeaderMenu />
+      {isLoggedIn && (
+        <>
+          <Header />
+          <HeaderMenu />
+        </>
+      )}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Login onLogin={handleLogin} />} // Muestra el componente Login
+        />
         <Route path="/detail/:productId" element={<Detail />} />
-        <Route path="/store" element={<Store/>}/>
+        <Route path="/store" element={<Store />} />
+        <Route path="/home" element={<Home />} /> 
       </Routes>
       <Footer />
     </div>
