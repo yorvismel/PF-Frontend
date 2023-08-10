06e07b3a7b7cebb177c016bb53daad18de4bcadd
Th0mcar <th0mcar@outlook.com>
2023-08-10 17:34:00 -0300

    Store arreglado, login raro eliminado y en progreso

--- PF-FrontEnd/src/components/Cards/Cards.jsx
+++ PF-FrontEnd/src/components/Cards/Cards.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './Cards.css';
-import Rating from 'react-rating-stars-component';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import "./Cards.css";
+import Rating from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../../Redux/actions";
 
 const Cards = () => {
+  const dispatch = useDispatch();
   const allProducts = useSelector((state) => state.products);
   const searchProduct = useSelector((state) => state.searchProduct);
- 
+  const [currentPage, setCurrentPage] = useState(1); // Agregamos el estado de la página actual
+console.log();
+  useEffect(() => {
+    console.log("Fetching products...");
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   const searchTerm = searchProduct.trim().toLowerCase();
   const filteredProducts = searchTerm
@@ -16,8 +23,13 @@ const Cards = () => {
       )
     : allProducts;
 
-  console.log("Filtered Products:", filteredProducts.map((product) => product.title));
-  console.log("Received Products:", allProducts); // Agrega este log para verificar los productos recibidos desde el store
+  const itemsPerPage = 15; // Definimos la cantidad de tarjetas por página
+  const totalItems = filteredProducts.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const visibleProducts = filteredProducts.slice(startIndex, endIndex);
 
   const handleRatingChange = (newRating) => {
     console.log(`El usuario ha revisado con ${newRating} estrellas`);
@@ -98,66 +110,3 @@ const Cards = () => {
 };
 
 export default Cards;
-
-
-
-// import React, { useState } from 'react';
-// import Rating from 'react-rating-stars-component';
-// import { Link } from 'react-router-dom';
-// import './Cards.css';
-// import Pagination from '../Pagination/Pagination';
-
-// const itemsPerPage = 12; // Número de productos por página
-
-// const Cards = ({ products }) => {
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   const handleRatingChange = (newRating) => {
-//     // Aquí puedes implementar la lógica para enviar la calificación al servidor
-//     console.log(`El usuario ha revisado con ${newRating} estrellas`);
-//   };
-
-//   const handlePageChange = (pageNumber) => {
-//     setCurrentPage(pageNumber);
-//   };
-
-//   const indexOfLastItem = currentPage * itemsPerPage;
-//   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-//   const currentProducts = products.slice(indexOfFirstItem, indexOfLastItem);
-
-//   return (
-//     <div className="cards-container">
-//       {currentProducts.map((product) => (
-//         <div key={product.title} className="card">
-//           <Link to={`/detail/${product.id}`} className="card-link">
-//             <img src={product.image} alt={product.title} className="card-image" />
-//           </Link>
-//           <h3 className="card-title">{product.title}</h3>
-//           <div className="card-footer">
-//             <span className="card-price">${product.price}</span>
-//             <Rating
-//               count={5}
-//               value={product.rating.rate}
-//               size={24}
-//               onChange={handleRatingChange}
-//               a11y={true}
-//               isHalf={true}
-//               emptyIcon={<i className="far fa-star"></i>}
-//               halfIcon={<i className="fa fa-star-half-alt"></i>}
-//               fullIcon={<i className="fa fa-star"></i>}
-//               activeColor="#ffd700"
-//             />
-//           </div>
-//         </div>
-//       ))}
-//       <Pagination
-//         itemsPerPage={itemsPerPage}
-//         totalItems={products.length}
-//         currentPage={currentPage}
-//         onPageChange={handlePageChange}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Cards;
